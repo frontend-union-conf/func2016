2af389e48a71012ab5242b243e7c8f2bc1588b9a
Nataliya Karatkova <nataliyakaratkova@gmail.com>
2016-08-01 04:04:56 +0300

    All sections

--- assets/scripts/bundle.js
+++ assets/scripts/bundle.js
@@ -1,18 +1,11 @@
-import SimpleMasonry from 'simple-masonry';
 import smoothScroll from 'smooth-scroll';
 
 'use strict';
-
-let simpleM = new SimpleMasonry({
-  masonryBox: '.speakers__columns',
-  masonryColumn: '.speakers__column'
-});
 
 document.addEventListener("DOMContentLoaded", function(event) {
   smoothScroll.init({
     updateURL: false
   });
-  simpleM.init();
 });
 
 (() => {
@@ -49,5 +42,87 @@ navToogle.addEventListener("click",function(e){
   navToogle.classList.toggle('header__toggle--open');
   nav.classList.toggle('header__navigation--open');
 },false);
+
+let map;
+
+let mapStyle = [{
+  "featureType": "administrative",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#444444"
+  }]
+}, {
+  "featureType": "landscape",
+  "elementType": "all",
+  "stylers": [{
+    "color": "#f2f2f2"
+  }]
+}, {
+  "featureType": "landscape",
+  "elementType": "geometry.fill",
+  "stylers": [{
+    "color": "#f4f5f7"
+  }]
+}, {
+  "featureType": "poi",
+  "elementType": "all",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "featureType": "road",
+  "elementType": "all",
+  "stylers": [{
+    "saturation": -100
+  }, {
+    "lightness": 45
+  }]
+}, {
+  "featureType": "road",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#271000"
+  }]
+}, {
+  "featureType": "water",
+  "elementType": "all",
+  "stylers": [{
+    "color": "#e3e6f4"
+  }]
+}];
+
+let mapIsDraggable = window.innerWidth > 480 ? true : false;
+let mapLatlng = {lat: 54.697426, lng: 25.309395};
+let mapOptions = {
+  zoom: 16,
+  draggable: mapIsDraggable,
+  center: mapLatlng,
+  scrollwheel: false,
+  streetViewControl: false,
+  mapTypeControl: false,
+  styles: mapStyle
+};
+let markerIcon = './assets/images/marker.png'
+let marker = new google.maps.Marker({
+  position: mapLatlng,
+  icon: markerIcon
+});
+
+if (document.getElementById('map')){
+  google.maps.event.addDomListener(window, 'load', initMap);
+  google.maps.event.addDomListener(window, "resize", changeWidth);
+}
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  marker.setMap(map);
+}
+
+function changeWidth() {
+  var center = map.getCenter();
+  google.maps.event.trigger(map, "resize");
+  map.setCenter(center);
+  map.setOptions({draggable: mapIsDraggable});
+}
 
 })();
